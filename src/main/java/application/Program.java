b0dd3846eecd4964684f1d3787fb5baa95cb137a
Joao Alexandre <jo.alexandre.ber@gmail.com>
2026-02-19 10:39:41 -0300

    Seller entity class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -9,5 +12,9 @@ public class Program {
         Department obj = new Department(1, "Books");
 
         System.out.println(obj);
+
+        Seller seller = new Seller(3000.00, new Date(), obj, "bob@gmail.com", 21, "Bob");
+
+        System.out.println(seller);
     }
 }
