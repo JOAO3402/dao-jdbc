dd7fb01bdc46a28b61b56c9db6f483787b0be6f1
Joao Alexandre <jo.alexandre.ber@gmail.com>
2026-02-20 12:36:23 -0300

    insert implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,8 +2,10 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -27,5 +29,12 @@ public class Program {
         for(Seller obj: list){
             System.out.println(obj);
         }
+
+        Department department = new Department(1, "Book");
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(4000.0, new Date(), department, "greg@gmailcom", null, "Greg");
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New Id = " + newSeller.getId());
     }
 }
